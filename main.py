8fccf0b7c51a932dae1221d0f384e320abd69c96
JustIanJ <tijkbotgames@gmail.com>
2022-05-09 08:57:08 +0200

    made a basic bot
    Co-authored-by: codeman1o1 <codeman1o1@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,13 @@
+import nextcord
+from nextcord.ext import commands
+from dotenv import load_dotenv
+import os
+
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+load_dotenv()
+bot.run(os.getenv("BOT_TOKEN"))
