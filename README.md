fae86e21638521d9605737f7d4825fb305881026
codeman1o1 <70335544+codeman1o1@users.noreply.github.com>
2022-05-16 16:19:50 +0200

    Fixed pretty much everything

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# TIJK-Bot Games is made and maintained by JustIanJ and codeman1o1.
-# Website: https://tijk-bot-games.github.io/TIJK-Bot-Games
+# TIJK-Bot Games is made and maintained by JustIanJ and codeman1o1
+
+## Website: <https://tijk-bot-games.github.io/TIJK-Bot-Games>
+
+Note:
+Some code is used from [the TIJK Bot repository](https://github.com/codeman1o1/TIJK-Bot).
+This is done with the permission from [the owner](https://github.com/codeman1o1)
---
+++ slash/general.py
@@ -0,0 +1,28 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction, slash_command as slash
+from views.buttons.link import Link
+
+from main import SLASH_GUILDS
+
+
+class General(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @slash(guild_ids=SLASH_GUILDS)
+    async def github(self, interaction: Interaction):
+        """Send a link to the official TIJK Bot Games GitHub page"""
+        embed = nextcord.Embed(color=0x0DD91A)
+        embed.add_field(
+            name="View the official TIJK Bot Games code now!",
+            value="https://github.com/TIJK-Bot-Games/TIJK-Bot-Games",
+            inline=False,
+        )
+        await interaction.response.send_message(
+            embed=embed, view=Link("https://github.com/TIJK-Bot-Games/TIJK-Bot-Games")
+        )
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(General(bot))
