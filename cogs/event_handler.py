160efbac48700495682e1ebdc378392a173da0ac
JustIanJ <iaajansma@gmail.com>
2022-05-16 08:47:34 +0200

    added event handler for later commands

---
+++ cogs/event_handler.py
@@ -0,0 +1,9 @@
+import nextcord
+import os
+from dotenv import load_dotenv
+from nextcord.ext import commands
+load_dotenv()
+
+# GRAB THE API TOKEN FROM THE .ENV FILE.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix=".")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # TIJK-Bot Games is made and maintained by JustIanJ and codeman1o1
 
-import os
 import nextcord
+import os
 from nextcord.ext import commands
 from dotenv import load_dotenv
 
