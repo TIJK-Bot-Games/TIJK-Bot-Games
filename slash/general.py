2c2072e6a68acf0b36d78b945fcee97154db968a
JustIanJ <iaajansma@gmail.com>
2022-07-01 15:03:30 +0200

    added command: /avatar

--- slash/general.py
+++ slash/general.py
@@ -25,6 +25,42 @@ class General(commands.Cog):
         )
 
     @slash(guild_ids=SLASH_GUILDS)
+    async def avatar(self, ctx, *, user: nextcord.Member = None):
+        """ Get the avatar of you or someone else """
+        user = user or ctx.author
+
+        avatars_list = []
+
+        def target_avatar_formats(target):
+            formats = ["JPEG", "PNG", "WebP"]
+            if target.is_animated():
+                formats.append("GIF")
+            return formats
+
+        if not user.avatar and not user.guild_avatar:
+            return await ctx.send(f"**{user}** has no avatar set, at all...")
+
+        if user.avatar:
+            avatars_list.append("**Account avatar:** " + " **-** ".join(
+                f"[{img_format}]({user.avatar.replace(format=img_format.lower(), size=1024)})"
+                for img_format in target_avatar_formats(user.avatar)
+            ))
+
+        embed = nextcord.Embed(colour=user.top_role.colour.value)
+
+        if user.guild_avatar:
+            avatars_list.append("**Server avatar:** " + " **-** ".join(
+                f"[{img_format}]({user.guild_avatar.replace(format=img_format.lower(), size=1024)})"
+                for img_format in target_avatar_formats(user.guild_avatar)
+            ))
+            embed.set_thumbnail(url=user.avatar.replace(format="png"))
+
+        embed.set_image(url=f"{user.display_avatar.with_size(256).with_static_format('png')}")
+        embed.description = "\n".join(avatars_list)
+
+        await ctx.send(f"🖼 Avatar to **{user}**", embed=embed)
+
+    @slash(guild_ids=SLASH_GUILDS)
     async def about(self, interaction: Interaction):
         """Tells you what TIJK Bot Games is."""
         embed = nextcord.Embed(color=0x0DD91A)
