bf1580cf8388951593d3e97a0c9fb86c70357afd
JustIanJ <iaajansma@gmail.com>
2022-07-01 10:00:42 +0200

    added developer.py for new commands in the near future

---
+++ slash/developer.py
@@ -0,0 +1,16 @@
+import nextcord
+from nextcord.application_command import SlashOption
+from nextcord import Interaction, slash_command as slash
+from views.buttons.link import Link
+from views.buttons.renew import Renew
+from nextcord.ext import commands
+from main import SLASH_GUILDS
+
+
+class Developer(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Developer(bot))
